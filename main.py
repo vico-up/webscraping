062afe23ac2e15c8520b8faacd4ed6b9843bb53f
Victor Gareca <victormartinez.vico@gmail.com>
2022-10-06 22:55:08 -0400

    ya realize la traduccion del titulo

--- main.py
+++ main.py
@@ -1,19 +1,72 @@
 
 """
 tareas:
-* Insertar detalles a txt
+* la lista guarda todos las caracteristicas de varias herramientas 
+    solucionar y poner saltos de linea
 
 """
-
-from email import header
-import os
-from urllib import request
-from wsgiref import headers
+from turtle import ScrolledCanvas
 import requests
 from bs4 import BeautifulSoup
+from googletrans import Translator
 import pandas as pd
+import os
 
-multi_line_string = """TITULO\n
+
+def nombreCarpeta():
+    for element in titulo:
+        titulito = element.find('h1', class_= "product_title entry-title single-post-title").getText('', strip = True)
+    
+        recortarCaracteres =  str(titulito)
+        eliminarCaracteres = "/″"
+        nombreCarpeta = ''.join(x for x in recortarCaracteres if x not in eliminarCaracteres)
+        return nombreCarpeta
+
+def nombreFoto():
+    for element in fichaProductos2:
+        foto = element.find('img', class_ = 'attachment-shop_single size-shop_single wp-post-image').get('src')
+        recortarCaracteres = str(foto)
+        caracteres = "http://www.tolsentools.com/wp-content/uploads/jpg"
+        recortarNombre = ''.join(x for x in recortarCaracteres if x not in caracteres)
+        nombreImagen = recortarNombre[6:]
+        return nombreImagen
+
+def crearCarpeta(nueva_ruta):
+    rutaFoto = f"{nueva_ruta}/{nombresitoFoto} - {nombresitoCarpeta}"
+    try:
+        os.mkdir(rutaFoto, mode=0o777)
+    except OSError:
+        print('fallo')   
+    else:
+        print('okis')
+    return rutaFoto
+
+        
+def crearFoto():
+    for element in fichaProductos2:
+        foto = element.find('img', class_ = 'attachment-shop_single size-shop_single wp-post-image').get('src')
+        img = requests.get(foto, headers = {"User-Agent": "Chrome/50.0.2661.94"})
+    
+    with open(f"{rutaCarpetita}/{nombresitoFoto}.jpg", 'wb') as imagen:
+        imagen.write(img.content)
+
+def tituloDetalles():
+    for element in titulo:
+        titulito = element.find('h1', class_= "product_title entry-title single-post-title").getText('', strip = True)
+        return titulito
+
+def crearArchivo():
+    with open(f"{rutaCarpetita}/detalles.txt", "a", encoding="UTF-8") as file:
+        file.write(f"{tituloEsp}")
+        file.write(f"{listaDetallesHerram}")
+
+def leerDatosHerramienta():
+    for element in titulo:
+        datitos = element.find('p').getText('', strip = True)       
+        return datitos
+
+
+multi_line_string = """\n
 Precio:\n
 Caracateristicas:\n
 🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠🛠
@@ -25,12 +78,15 @@ https://wa.me/c/59174810534
 Enlaces:\n
 Etiquetas herramientas:\n
 Tolsen, Herramientas, Construccion, Mecanico, taller, Drywall\n
-"""
+"""  
+traducirTitulo = None
+datitosHerramienta = None
+listaDetallesHerram = []
+tituloEsp = None
+translator = Translator()
 
+direccionMadre = "C:/Users/Victor/OneDrive/ventas/Tolsen/Set de herramientas"
 categoriaProducto = 'tool-set'
-contadorPagina = 1
-listaImagen = list()
-listaProducto = list()
 
 urlCategoria = (f'http://www.tolsentools.com/product-catagories/{categoriaProducto}/page/2/')
 r = requests.get(urlCategoria, headers = {'User-Agent': "Chrome/50.0.2661.94"})
@@ -41,56 +97,27 @@ fichaProductos = soup.find_all('div', class_ = 'product-inner clr')
 cont = 0
 
 for element in fichaProductos:
-    imagenProducto = element.find('img', class_ = 'woo-entry-image-main').get('src')
-    nombreProducto = element.find('div', class_ = 'product-details match-height-content').getText('', strip = True)
+    enlace = element.find('a', class_ = 'woocommerce-LoopProduct-link woocommerce-loop-product__link').get('href')
+    r2 = requests.get(enlace, headers={'User-Agent': "Chrome/50.0.2661.94"})
+    html2 = r2.content
+    soup2 = BeautifulSoup(html2, 'html.parser')
+    titulo = soup2.find_all("div", class_= "summary entry-summary")
+    fichaProductos2 = soup2.find_all('div', class_ = "woocommerce-product-gallery__image")
+    datosHerramienta = soup2.find_all('div', class_ = "woocommerce-product-details__short-description")
+    nombresitoCarpeta = nombreCarpeta()
+    nombresitoFoto = nombreFoto()
+    rutaCarpetita = crearCarpeta(direccionMadre)
+    crearFoto()
+    titulitoDetalles = tituloDetalles()
+    traducirTitulo = translator.translate(f'{titulitoDetalles}', src='en', dest='es')
+    tituloEsp = traducirTitulo.text
+    datitosHerramienta = leerDatosHerramienta()
+    listaDetallesHerram.append(datitosHerramienta)
+    crearArchivo()
+    print(listaDetallesHerram)
 
+    cont = cont + 1 
     
-    #print(type(nombreProducto))
-    cont = cont + 1
-
-    img = requests.get(imagenProducto, headers = {"User-Agent": "Chrome/50.0.2661.94"})
-    recortarCaracteres = str(imagenProducto)
-    recortarCaracteres2 = str(nombreProducto)
-    
-    
-    
-    caracteres = "http://www.tolsentools.com/wp-content/uploads/jpg"
-    caracteres2 = "/″"
-
-    recortarNombre2 = ''.join(x for x in recortarCaracteres2 if x not in caracteres2)
-    recortarNombre = ''.join(x for x in recortarCaracteres if x not in caracteres)
-    nombreImagen = recortarNombre[6:] + " - " + recortarNombre2 + ".jpg"
-
-    NombreCarpeta = recortarNombre[6:] + " - " + recortarNombre2
-
-    print(imagenProducto)
-    print(recortarNombre2)
-
-    try:
-        os.mkdir(f"C:/Users/Victor/OneDrive/ventas/Tolsen/Set de herramientas/{NombreCarpeta}")
-
-        with open(f"C:/Users/Victor/OneDrive/ventas/Tolsen/Set de herramientas/{NombreCarpeta}/{nombreImagen}", 'wb') as imagen:
-            imagen.write(img.content)
-
-        with open(f"C:/Users/Victor/OneDrive/ventas/Tolsen/Set de herramientas/{NombreCarpeta}/Detalles.txt", 'w', encoding = 'utf-8') as file:            
-            file.write(multi_line_string)
-
-        
-
-    except OSError:
-        print('fallo')
-
-    else:
-        print('okis')
-
-
-
-
-    
-    #with open(nombreImagen, 'wb') as imagen:
-      #  imagen.write(img.content)
-
-#crearDirectorio = f'C:/Users/Victor/OneDrive/Proyectos/Programacion/webscraping whit python/{nombreCarpeta}'
 
 
 
--- pruebistas.py
+++ pruebistas.py
@@ -1,7 +1,58 @@
+#Creo que la clave esta en que 2 funciones me devuelvav los nombre y otras que me creen las rutas
+
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
+def nombreCarpeta():
+    for element in titulo:
+        titulito = element.find('h1', class_= "product_title entry-title single-post-title").getText('', strip = True)
+    
+        recortarCaracteres =  str(titulito)
+        eliminarCaracteres = "/″"
+        nombreCarpeta = ''.join(x for x in recortarCaracteres if x not in eliminarCaracteres)
+        return nombreCarpeta
+
+def nombreFoto():
+    for element in fichaProductos2:
+        foto = element.find('img', class_ = 'attachment-shop_single size-shop_single wp-post-image').get('src')
+        recortarCaracteres = str(foto)
+        caracteres = "http://www.tolsentools.com/wp-content/uploads/jpg"
+        recortarNombre = ''.join(x for x in recortarCaracteres if x not in caracteres)
+        nombreImagen = recortarNombre[6:]
+        return nombreImagen
+
+def crearCarpeta(nueva_ruta):
+    rutaFoto = f"{nueva_ruta}/{nombresitoFoto} - {nombresitoCarpeta}"
+    try:
+        os.mkdir(rutaFoto, mode=0o777)
+    except OSError:
+        print('fallo')   
+    else:
+        print('okis')
+    return rutaFoto
+
+        
+def crearFoto():
+    for element in fichaProductos2:
+        foto = element.find('img', class_ = 'attachment-shop_single size-shop_single wp-post-image').get('src')
+        img = requests.get(foto, headers = {"User-Agent": "Chrome/50.0.2661.94"})
+    
+    with open(f"{rutaCarpetita}/{nombresitoFoto}.jpg", 'wb') as imagen:
+        imagen.write(img.content)
+
+def leerDatosHerramienta():
+    for element in leerDatosHe:
+        datitos = element.find('p').getText('', strip = True)       
+        datosStr = str(datitos)
+        return datosStr
+        
+
+
+
+direccionMadre = "C:/Users/Victor/OneDrive/ventas/Tolsen/Set de herramientas"
 categoriaProducto = 'tool-set'
 
 urlCategoria = (f'http://www.tolsentools.com/product-catagories/{categoriaProducto}/page/2/')
@@ -17,18 +68,29 @@ for element in fichaProductos:
     r2 = requests.get(enlace, headers={'User-Agent': "Chrome/50.0.2661.94"})
     html2 = r2.content
     soup2 = BeautifulSoup(html2, 'html.parser')
+    titulo = soup2.find_all("div", class_= "summary entry-summary")
     fichaProductos2 = soup2.find_all('div', class_ = "woocommerce-product-gallery__image")
-
-    cont2 = 0
-
-    for element in fichaProductos2:
-        foto = element.find('img', class_ = 'attachment-shop_single size-shop_single wp-post-image').get('src')
-
-        cont2 = cont2 + 1
-        img = requests.get(foto, headers = {"User-Agent": "Chrome/50.0.2661.94"})
-
+    
+    enlace2 = element.find('div', class_ = 'woocommerce-LoopProduct-link woocommerce-loop-product__link')
+    r3 = requests.get(enlace, headers={'User-Agent': "Chrome/50.0.2661.94"})
+    html3 = r3.content
+    soup3 = BeautifulSoup(html3, 'html.parser')
+    leerDatosHe = soup3.find_all('div', class_ = "woocommerce-product-details__short-description")
+    
+    nombresitoCarpeta = nombreCarpeta()
+    nombresitoFoto = nombreFoto()
+    #rutaCarpetita = crearCarpeta(direccionMadre)
+    #crearFoto()
+    datitosHerramientas = leerDatosHerramienta()
+    print(type(datitosHerramientas))
+    
+    
     cont = cont + 1 
-    print(img)
+    
+
+
+
+
 
 
 
